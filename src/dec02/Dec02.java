29141ed03d5d598d36c319ff8597698bd74ace87
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-02 08:27:06 -0400

    Dec02 solutions

--- src/dec02/Dec02.java
+++ src/dec02/Dec02.java
@@ -2,14 +2,21 @@ package dec02;
 
 import framework.AOCParent;
 
+import java.util.List;
+
 public class Dec02 extends AOCParent {
+
+    public static List<Range> ranges = InputLoader.loadRanges();
+
     @Override
     public void part1() {
-
+        long sum = ranges.stream().mapToLong(Range::computeInvalidIdsSumPart1).sum();
+        printSolution(sum);
     }
 
     @Override
     public void part2() {
-
+        long sum = ranges.stream().mapToLong(Range::computeInvalidIdsSumPart2).sum();
+        printSolution(sum);
     }
 }
