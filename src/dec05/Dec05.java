966bb9f8b5b707cb4f2d14b17c270d8476cf198c
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-04 08:04:11 -0400

    Prep for Dec05

---
+++ src/dec05/Dec05.java
@@ -0,0 +1,20 @@
+package dec05;
+
+import framework.AOCParent;
+
+public class Dec05 extends AOCParent {
+    @Override
+    public void loadInput() {
+
+    }
+
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+}
---
+++ src/dec05/InputLoader.java
@@ -0,0 +1,6 @@
+package dec05;
+
+import framework.InputLoaderParent;
+
+public class InputLoader extends InputLoaderParent {
+}
