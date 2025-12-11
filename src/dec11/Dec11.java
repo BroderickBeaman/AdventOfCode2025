bb3ad936779cb844f0214e9b94d30daa07d9491b
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-11 08:12:54 -0400

    Dec11 part 1 solution

--- src/dec11/Dec11.java
+++ src/dec11/Dec11.java
@@ -2,15 +2,56 @@ package dec11;
 
 import framework.AOCParent;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Dec11 extends AOCParent {
+
+    private Map<String, Set<String>> serverRack;
+    private Map<String, Long> pathCache;
+
     @Override
     public void loadInput() {
-
+        serverRack = InputLoader.loadServerRack();
     }
 
     @Override
     public void part1() {
+        pathCache = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        visited.add("you");
+        printSolution(numPathsAtNode("you", visited));
+    }
 
+    private long numPathsAtNode(String current, Set<String> visited) {
+        if (pathCache.containsKey(current)) {
+            return pathCache.get(current);
+        }
+
+        if (current.equals("out")) {
+            return 1;
+        }
+
+        if (!serverRack.containsKey(current)) {
+            pathCache.put(current, 0L);
+            return 0L;
+        }
+
+        Set<String> currentVisited = new HashSet<>(visited);
+        currentVisited.add(current);
+        long tally = 0;
+        for (String connected : serverRack.get(current)) {
+            // Prevent infinite loop
+            if (currentVisited.contains(connected)) {
+                continue;
+            }
+            tally += numPathsAtNode(connected, currentVisited);
+        }
+
+        pathCache.put(current, tally);
+        return tally;
     }
 
     @Override
--- src/dec11/InputLoader.java
+++ src/dec11/InputLoader.java
@@ -2,5 +2,21 @@ package dec11;
 
 import framework.InputLoaderParent;
 
+import java.util.*;
+
 public class InputLoader extends InputLoaderParent {
+
+    public static Map<String, Set<String>> loadServerRack() {
+        Map<String, Set<String>> map = new HashMap<>();
+        for (String line : loadLines()) {
+            String[] parts = line.split(" ");
+            String key = parts[0].substring(0, parts[0].length() - 1);
+            Set<String> values = new HashSet<>();
+            for (int i = 1; i < parts.length; i++) {
+                values.add(parts[i]);
+            }
+            map.put(key, values);
+        }
+        return map;
+    }
 }
